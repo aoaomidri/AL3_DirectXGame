cd00a0b79ff72ad4eda4c748fd2e13f6139e29e0
aoaomidri <aoaomidori2723@gmail.com>
2023-06-26 14:40:20 +0900

    skyDome.h,.cpp追加

---
+++ SkyDome.cpp
@@ -0,0 +1,17 @@
+#include "SkyDome.h"
+#include <assert.h>
+SkyDome::SkyDome() { worldTransform_.translation_ = {0, 0, 0}; }
+
+void SkyDome::Initialize(Model* model, uint32_t textureHandle) {
+	// ぬるぽチェック
+	assert(model);
+	// メンバ変数に記録
+	model_ = model;
+	textureHandle_ = textureHandle;
+
+	worldTransform_.Initialize();
+}
+
+void SkyDome::Update() { worldTransform_.UpdateMatrix(scale); }
+
+void SkyDome::Draw(ViewProjection viewProjection) { model_->Draw(worldTransform_, viewProjection); }
---
+++ SkyDome.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "Model.h"
+#include "WorldTransform.h"
+
+class SkyDome {
+public:
+	SkyDome();
+
+	void Initialize(Model* model, uint32_t textureHandle);
+
+	void Update();
+
+	void Draw(ViewProjection viewProjection);
+
+private:
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+	// モデル
+	Model* model_ = nullptr;
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+
+	float size = 800.0f;
+
+	Vector3 scale = {size, size, size};
+};
