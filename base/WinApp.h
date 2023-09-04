e30453e005ca1b82bf51b4e2666e47e54f92ddd6
aoaomidri <aoaomidori2723@gmail.com>
2023-09-04 18:11:53 +0900

    完了補げ

--- base/WinApp.h
+++ base/WinApp.h
@@ -45,7 +45,7 @@ public: // メンバ関数
 	/// <param name="clientHeight">ウィンドウのクライアント領域の初期高さ</param>
 	/// </summary>
 	void CreateGameWindow(
-	    const wchar_t* title = L"LE2A_20_ムラカミ_アオイ_AL3", UINT windowStyle = WS_OVERLAPPEDWINDOW,
+	    const wchar_t* title = L"LE2A_20_ムラカミ_アオイ_アームシューター", UINT windowStyle = WS_OVERLAPPEDWINDOW,
 	    int32_t clientWidth = kWindowWidth, int32_t clientHeight = kWindowHeight);
 
 	/// <summary>
